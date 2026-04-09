aebe61f744ce44f2b2d78342e15e5338473face6
Abhinav Bombale <bombaleabhinav@gmail.com>
2026-04-09 17:44:42 +0530

    chore: prepare backend for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -19,6 +19,10 @@ app.get('/', (req, res) => {
   res.send('GitAtlas Backend is running🚀');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
